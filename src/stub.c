c0e2cb55da8b8ef295bd9b67c16dfa7b3c7923a8
John Hester <hesterj@etableau.com>
2025-06-01 21:41:41 -0500

    wip: use ctypes

--- src/stub.c
+++ src/stub.c
@@ -1,6 +1,10 @@
 #include "stub.h"
 #include "ta_libc.h"
+#include "ta_abstract.h"
 
 int add(int a, int b) {
     return a + b;
 }
+
+TA_LIB_API TA_RetCode TA_GroupTableAlloc( TA_StringTable **table );
+TA_LIB_API TA_RetCode TA_GroupTableFree ( TA_StringTable *table );
--- src/ta_lib.ml
+++ src/ta_lib.ml
@@ -1,6 +1,10 @@
+module TA = struct
+  module GroupTable = struct
+    type t
 
-module Test = struct
+    (* external alloc : unit -> t = "TA_GroupTableAlloc" *)
+  end
 
-  external add : int -> int -> int = "add"
-
+  (* external add : int -> int -> int = "add" *)
+  let add _ _ = 0
 end

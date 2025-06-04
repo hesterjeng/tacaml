c348d28a0a06052768c5a0c7139a5f45229ccd07
John Hester <hesterj@etableau.com>
2025-06-04 11:05:16 -0500

    chore: remove old states

--- src/stub.c
+++
@@ -1,7 +0,0 @@
-#include "stub.h"
-#include "ta_libc.h"
-#include "ta_abstract.h"
-
-int add(int a, int b) {
-    return a + b;
-}
--- src/stub.h
+++
@@ -1,6 +0,0 @@
-#ifndef MY_C_CODE_H
-#define MY_C_CODE_H
-
-int add(int a, int b);
-
-#endif

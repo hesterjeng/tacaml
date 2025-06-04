4f164d22bf6303e85f9d7a2f770dfc99dfd5a0df
John Hester <hesterj@etableau.com>
2025-06-04 11:04:29 -0500

    feat: use stubs on sma

--- bin/tacaml.ml
+++ bin/tacaml.ml
@@ -1,7 +1,20 @@
-module T = Tacaml__Ta_lib
+module TA = Tacaml__Ta_lib
+module Arr = Tacaml__Arr
+
+let top () =
+  let ( let* ) = Result.( let* ) in
+  let data = Arr.init 1000 @@ fun i -> Float.of_int i in
+  (* let output = Arr.create 1000 in *)
+  let* () = TA.initialize () in
+  (* let* () = TA.sma 999 999 2 data output in *)
+  let* res = TA.sma_top 2 data in
+  Format.printf "%f" res;
+  (* Format.printf "%a" Arr.pp output; *)
+  Result.return @@ ()
 
 let () =
-  T.TA.Common.initialize ();
-  let x = T.TA.GroupTable.allocate () in
-  Format.printf "@[%a@]@." T.TA.GroupTable.pp x;
-  ()
+  match top () with
+  | Ok () ->
+    Format.printf "@[OK@]@.";
+    ()
+  | Error e -> Format.printf "@[retcode %d@]@." e

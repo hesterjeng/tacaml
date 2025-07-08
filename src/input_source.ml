5598d79787ec5d1ef226f604c19b74f41585c9ec
John Hester <hesterj@etableau.com>
2025-07-08 00:58:36 -0500

    feat: input flags

--- src/input_source.ml
+++ src/input_source.ml
@@ -4,3 +4,7 @@ type t =
   | Ohlcv of Ohlcv.t
   | FloatBA of float_ba
   | FloatBA2 of float_ba * float_ba
+
+module Flag = struct
+  type t = OhlcvFlag | FloatBAFlag | FloatBA2Flag
+end
--- src/output_destination.ml
+++ src/output_destination.ml
@@ -6,3 +6,12 @@ type t =
   | FloatBA3 of float_ba * float_ba * float_ba
   | IntBA of int_ba
   | IntBA2 of int_ba * int_ba
+
+module Flag = struct
+  type t =
+    | FloatBAFlag of Type.t
+    | FloatBA2Flag of Type.t * Type.t
+    | FloatBA3Flag of Type.t * Type.t * Type.t
+    | IntBAFlag of Type.t
+    | IntBA2Flag of Type.t * Type.t
+end

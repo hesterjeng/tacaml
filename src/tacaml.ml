373ea8adc350329d0ae4f4e895dbb8951c5107c8
John Hester <hesterj@etableau.com>
2025-07-07 21:52:51 -0500

    feat: packed values at toplevel

--- src/tacaml.ml
+++ src/tacaml.ml
@@ -1,8 +1,12 @@
 module Ohlcv = Ohlcv
-module Wrappers = Wrappers
-module Raw = C.Functions
-module Output = Output
+module C = C.Functions
 module Pack = Pack
 module Input_source = Input_source
 module Output_destination = Output_destination
 module Type = Type
+module Defaults = Defaults
+
+type t = Pack.t
+
+let calculate = Pack.calculate
+let all = Defaults.all

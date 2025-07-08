7d51df246f2c541e2827147d5f78cc14b37dbfda
John Hester <hesterj@etableau.com>
2025-07-07 22:13:42 -0500

    chore: module naming

--- src/tacaml.ml
+++ src/tacaml.ml
@@ -1,10 +1,10 @@
 module Ohlcv = Ohlcv
 module C = C.Functions
-module Pack = Pack
-module Input_source = Input_source
-module Output_destination = Output_destination
-module Type = Type
+module Safe = Wrappers
+module Input = Input_source
+module Output = Output_destination
 module Defaults = Defaults
+module Indicator = Type
 
 type t = Pack.t
 
